b81859bfc1179fa5edf6570f077112db6400da76
olivier <olivier.laffon@epitech.eu>
2022-05-09 08:54:29 +0200

    Added exchange simulation via button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 import Game from './context/Game';
 
-function App() {
+
+const App = () => {
+  const [ wins, setWins ] = useState<string[]>([]);
+  const [ nameP1, setNameP1 ] = useState('P1');
+  const [ nameP2, setNameP2 ] = useState('P2');
+
+  const handleAddP1Click = () => {
+    console.log('Added p1');
+    wins.push(nameP1);
+    setWins([...wins]);
+  }
+  const handleAddP2Click = () => {
+    wins.push(nameP2);
+    setWins([...wins]);
+  }
+  
   return (
     <div className="App">
-      <Game nameP1='P1' nameP2='P2' wins={['P1']}/>
+      <Game nameP1={nameP1} nameP2={nameP2} wins={wins}/>
+      <button onClick={handleAddP1Click}>add p1 win exchange</button>
+      <button onClick={handleAddP2Click}>add p2 win exchange</button>
     </div>
   );
 }
